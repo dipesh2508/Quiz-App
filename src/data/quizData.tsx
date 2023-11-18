a4b173047caacff8102009cf11d22eb2af99afea
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-11-19 03:18:09 +0530

    added quiz data

---
+++ src/data/quizData.tsx
@@ -0,0 +1,230 @@
+export const QuizData = [{
+    options: ['Pokemon', 'Kids Anime/Cartoon (Nostalgia)', 'Anime', 'Marvel/DC'],
+    data: [
+        [{
+            question: 'Which Pokemon has the ability "Levitate," making it immune to Ground-type moves?',
+            options: ['Gengar', 'Flygon', 'Crobat', 'Rotom'],
+            answer: 1
+        },
+        {
+            question: "Which Pokemon type is not super effective against any other type?",
+            options: ['Fairy', 'Normal', 'Psychic', 'Electric'],
+            answer: 2
+        },
+        {
+            question: "Which Pokemon is a dual-type Dragon/Flying Pokemon and is said to be the guardian of the skies?",
+            options: ['Rayquaza', 'Salamence', 'Dragonite', 'Altaria'],
+            answer: 1
+        },
+        {
+            question: "Which Pokemon has the highest base HP stat?",
+            options: ['Wobbuffet', 'Snorlax', 'Chansey', 'Blissey'],
+            answer: 4
+        },
+        {
+            question: 'In the Pokemon games, which type is super effective against both Grass and Electric types?',
+            options: ['Fire', 'Bug', 'Ground', 'Ice'],
+            answer: 3
+        },
+        {
+            question: 'What is the name of the Pokemon that can learn every move in the game?',
+            options: ['Mewtwo', 'Ditto', 'Smeargle', 'Arceus'],
+            answer: 3
+        },
+        {
+            question: 'Which Pokemon has the highest base Special Attack stat?',
+            options: ['Mewtwo', 'Alakazam', 'Gengar', 'Espeon'],
+            answer: 1
+        },
+        {
+            question: 'Which Pokemon type is immune to poison-type moves?',
+            options: ['fire', 'steel', 'dark', 'water'],
+            answer: 2
+        },
+        {
+            question: 'Garchomp is known for its exceptional speed and power. What is its base Speed stat?',
+            options: ['90', '100', '110', '120'],
+            answer: 3
+        },
+        {
+            question: 'What is the signature move of Typhlosion?',
+            options: ['Blast burn', 'Overheat', 'Fire blast', 'Flamethrower'],
+            answer: 1
+        },
+        {
+            question: ' In the Pokémon anime series, which Pokémon does Gary Oak evolve his Eevee into?',
+            options: ['Espeon', 'Leafeon', 'Vapereon', 'Umbreon'],
+            answer: 4
+        },
+        {
+            question: 'What is the signature Pokémon of Steven, the Champion of the Hoenn region?',
+            options: ['Aggron', 'Metagross', 'Skarmory', 'Claydol'],
+            answer: 2
+        },
+        {
+            question: 'What does STAB mean?',
+            options: ['Same Type Attack Boost', 'Special Technique Amplification Bonus', 'Same Type Attack Bonus', 'Super Effective Attack Boost'],
+            answer: 3
+        },
+            // {
+            //     question: "What was ash's first shiny pokemon",
+            //     options: ['Noctowl', 'Litten', 'Poipole', 'Butterfree'],
+            //     answer: 1
+            // }
+        ],
+        [{
+            question: "What is Doraemon's favorite food?",
+            options: ['Pizza', 'Sushi', 'Dorayaki', 'Pan Cakes'],
+            answer: 3
+        },
+        {
+            question: "What is the name of Shinchan's kindergarten?",
+            options: ['Futaba Kindergarten', 'Shijuku Kindergarten', 'Osaka kindergarten', 'Kasukabe Kindergarten'],
+            answer: 4
+        },
+        {
+            question: "What is the name of Kiteretsu's robotic cat?",
+            options: ['Mecha-Kiteretsu', 'Robo-Korosuke', 'Korobotsu', 'Daichan'],
+            answer: 2
+        },
+        {
+            question: "What is the name of Ben's first alien transformation in the original series?",
+            options: ['Heatblast', 'Swampfire', 'Wildmutt', 'Diamond Head'],
+            answer: 1
+        },
+        {
+            question: "What is the name of Manny's robotic pet dog? (Handy Manny)",
+            options: ['Fluffy', 'Whiskers', 'Flicker', 'Fix-It'],
+            answer: 4
+        },
+        {
+            question: "Who is the main villain in Phineas and Ferb, constantly trying to take over the Tri-State Area?",
+            options: ['Dr. Doofenshmirtz', 'Major Monogram', 'Jeremy Johnson', 'Candace'],
+            answer: 1
+        },
+        {
+            question: "What is the name of Tyson's Beyblade?",
+            options: ['Dranzer', 'Dragoon', 'Draciel', 'Driger'],
+            answer: 2
+        },
+        {
+            question: "Who is the main antagonist in Ben 10: Ultimate Alien?",
+            options: ['Vilgax', 'Albedo', 'Eon', 'Aggregor'],
+            answer: 4
+        },
+        {
+            question: "What is the name of Mickey Mouse's dog companion?",
+            options: ['Pluto', 'Max', 'Figaro', 'Goofy'],
+            answer: 1
+        },
+        {
+            question: "What is the name of the group of friends who solve mysteries together?",
+            options: ['Ghostbusters', 'Monster Hunters', 'Mystery Inc.', 'Sleuth Squad'],
+            answer: 3
+        }],
+        [
+            {
+                question: 'Which Character does Madara declares the strongest? (Naruto Shippuden)',
+                options: ['Obito Uchiha', 'Maito Gai', 'Kakashi Uchiha', 'Naruto Uzumaki'],
+                answer: 2
+            },
+            {
+                question: "What is the name of Ichigo's Zanpakuto? (Bleach)",
+                options: ['Zabimaru', 'Sode no Shirayuki', 'Hyorinmaru', 'Tensa Zangetsu'],
+                answer: 4
+            },
+            {
+                question: "What is the name of the legendary ship of Gol D. Roger? (One Piece)",
+                options: ['Going Merry', 'Oro Jackson', 'Red Force', 'Moby Dick'],
+                answer: 2
+            },
+            {
+                question: 'What is the hero name of Katsuki Bakugo? (Boku no Hero Academia)',
+                options: ['Great Explosion Murder God Dynamight', 'Blast Master', 'Nitro Dynamite', 'Explosion boy'],
+                answer: 1
+            },
+            {
+                question: "What is the name of the main protagonist in Code Geass? (Code Geass)",
+                options: ["Suzaku Kururugi", 'C.C.', 'Lelouch Lamperouge', 'Euphemia li Britannia'],
+                answer: 3
+            },
+            {
+                question: "What were the last words of Levi to Erwin Smith? (Attack on Titan)",
+                options: ["Erwin, I will fulfill your dream", 'Erwin, give up on your dreams and die', "Erwin, it's time to go. Your duty is complete", "Erwin, the choice is yours. What do you want to do?"],
+                answer: 4
+            },
+            {
+                question: ' What is the overall theme or message conveyed in "Your Name"?(Your Name)',
+                options: ["Fate and destiny", "Love transcending time and space", "The importance of memories", "The beauty of ordinary moments"],
+                answer: 2
+            },
+            {
+                question: "What is the name of Zenitsu's unique ability that allows him to move and attack at incredible speeds? (Kimetsu no Yaiba)",
+                options: ["Thunderclap", "Flash Step", "Lightning Strike", "Thunder Burst"],
+                answer: 1
+            },
+            {
+                question: "What is the name of L's personal assistant and guardian?",
+                options: ['Wammy', 'Yotsuba', 'Watari', 'Matsuda'],
+                answer: 3
+            },
+            {
+                question: "What is Gilgamesh's primary motivation and desire?",
+                options: ['to conquer the world', 'to find his lost love', 'to destroy mankind', 'to attain immortality'],
+                answer: 4
+            }
+        ],
+        [
+            {
+                question: 'In the "Spider-Man: Homecoming" film, which actor plays the character of the Vulture?',
+                options: ['Samuel L. Jackson', 'Michael Keaton', 'Jake Gyllenhaal', 'William Dafoe'],
+                answer: 2
+            },
+            {
+                question: "Who was the first Robin? (Batman)",
+                options: ['Jason Todd', 'Tim Drake', 'Dick Grayson', 'Damian Wayne'],
+                answer: 3
+            },
+            {
+                question: 'What is the name of the newspaper where Clark Kent works as a reporter? (Superman)',
+                options: ['Daily Planet', 'Daily Bugle', 'Daily Globe', 'Daily Star'],
+                answer: 1
+            },
+            {
+                question: "what is the name of Doctor Strange's loyal disciple and love interest, who eventually becomes a powerful sorceress herself?",
+                options: ['Jessica jones', 'Wanda Maximoff', 'Clea', 'Jane foster'],
+                answer: 3
+            },
+            {
+                question: 'What is the name of the all-female special forces unit in Wakanda, led by Okoye? (Black Panther)',
+                options: ['Jabari Tribe', 'Dora Milaje', 'War Dogs', 'Hunter womens'],
+                answer: 2
+            },
+            {
+                question: "According to the Flash Comics, which speedster becomes the fastest after Barry Allen's Death?",
+                options: ['Wally West', 'Nora West Allen', 'Eobard Thawne', 'Godspeed'],
+                answer:1
+            },
+            {
+                question: "What is Deadpool's primary mutant power?",
+                options: ['Teleportation', 'Telepathy','Super strength','Regenerative healing factor'],
+                answer: 4
+            },
+            {
+                question: 'Which character is known as the King of Dreams? (The Sandman, DC)',
+                options: ['Lucifer',' Constantine', 'Morpheus', 'Destiny'],
+                answer: 3
+            },
+            {
+                question: "Which terrorist organization kidnaps Tony Stark and demands that he builds them a powerful weapon? (Iron Man)",
+                options: ['A.I.M. (Advanced Idea Mechanics)', 'Ten Rings', 'HYDRA', 'The Hand'],
+                answer: 2
+            },
+            {
+                question: 'Who is the main antagonist in the "Secret Wars" storyline? ',
+                options: ['Doctor Doom', 'Thanos', 'Kang', 'Magneto'],
+                answer: 1
+            }
+        ]
+    ]
+}];
