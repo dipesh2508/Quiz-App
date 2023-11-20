dc37b5fb7b28fd76cc70b43ac4e769e2f21be2bb
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-11-20 14:22:23 +0530

    added barebones quiz container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import NavBar from "./components/navBar";
+import Quiz from "./components/quiz";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="fixed backdrop-blur-md top-44 left-0 h-full w-full -z-1"></div>
-      <main className="background"></main>
+      <div className="fixed backdrop-blur-md top-44 left-0 h-full w-full fix-it"></div>
+      <main className="background grid place-items-center">
+        <Quiz />
+      </main>
     </>
   );
 }
---
+++ src/components/quiz.tsx
@@ -0,0 +1,13 @@
+const Quiz = () => {
+  return (
+    <div className="over-the-top relative container bg-slate-300 w-1/3 h-96 rounded-3xl p-4 bg-opacity-80 flex flex-col gap-4">
+        <div className="h-32 p-4 border-2 border-pink-400 rounded-xl bg-opac">
+            Question
+        </div>
+    
+        <div className="h-8 border-2 border-sky-400 rounded-lg"></div>
+    </div>
+  )
+}
+
+export default Quiz
