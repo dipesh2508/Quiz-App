ffd691cff6f08fee4e5fbde344eec6eb5a6d1e2e
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-11-20 23:07:08 +0530

    added the container(container)

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -2,10 +2,13 @@ const NavBar = () => {
   return (
     <div
       className="grid h-44 w-full place-content-center 
-    bg-gradient-to-b from-sky-500 to-pink-500 
+    bg-gradient-to-r  from-pink-400 to-orange-400
     text-center font-serif text-6xl font-semibold text-slate-100"
     >
       React Quiz App
+      <div className="mt-4 font-sans text-xl text-slate-200">
+        Test Your Knowledge
+      </div>
     </div>
   );
 };
--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -1,13 +1,43 @@
 const Quiz = () => {
   return (
-    <div className="over-the-top relative container bg-slate-300 w-1/3 h-96 rounded-3xl p-4 bg-opacity-80 flex flex-col gap-4">
-        <div className="h-32 p-4 border-2 border-pink-400 rounded-xl bg-opac">
-            Question
+    <div className="over-the-top container relative flex h-96 w-1/3 flex-col items-center justify-center gap-2 rounded-3xl bg-slate-300 bg-opacity-80 p-4">
+      <div className="w-full flex-1">
+        <div className="h-32 rounded-xl border-2 border-pink-400 bg-opacity-80 p-4 drop-shadow-lg">
+          Question
         </div>
-    
-        <div className="h-8 border-2 border-sky-400 rounded-lg"></div>
-    </div>
-  )
-}
+      </div>
 
-export default Quiz
+      <div className="w-full flex-1">
+        <div className="h-8 rounded-lg border-2 border-sky-400 bg-opacity-80 drop-shadow-md hover:bg-sky-400"></div>
+      </div>
+      <button
+        className="group relative inline-flex h-8 w-1/3 items-center
+      justify-center overflow-hidden rounded-lg bg-gradient-to-r from-pink-400 to-orange-400 font-medium text-white shadow-md
+      transition duration-300 ease-out"
+      >
+        <span className="ease absolute inset-0 flex h-full w-full -translate-x-full items-center justify-center bg-gradient-to-r from-pink-400 to-orange-400 text-white duration-300 group-hover:translate-x-0">
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            ></path>
+          </svg>
+        </span>
+        <span className="ease absolute flex h-full w-full transform items-center justify-center text-slate-200 transition-all duration-300 group-hover:translate-x-full">
+          Button Text
+        </span>
+        <span className="invisible relative">Submit</span>
+      </button>
+    </div>
+  );
+};
+
+export default Quiz;
