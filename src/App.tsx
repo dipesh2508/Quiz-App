31ddc147dd713524e5e4bd46f3d9734542ef5b26
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-11-25 00:26:01 +0530

    added footbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import NavBar from "./components/navBar";
 import Quiz from "./components/quiz";
+import Footbar from "./components/footbar";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="fixed backdrop-blur-md top-44 left-0 h-full w-full fix-it"></div>
+      <div className="fixed backdrop-blur-md top-44 left-0 w-full fix-it"></div>
       <main className="background grid place-items-center">
         <Quiz />
       </main>
+      <Footbar />
     </>
   );
 }
---
+++ src/components/footbar.tsx
@@ -0,0 +1,12 @@
+const Footbar = () => {
+  return (
+    <div className="w-full bg-gradient-to-r  from-pink-400 to-orange-400 p-4 text-center text-slate-300">
+        Made with &#10084; by 
+        <span className=" font-semibold font-serif text-lg px-1 text-neutral-800">
+          Dipesh Ranjan
+        </span>
+      </div>
+  )
+}
+
+export default Footbar
